8d967345333f4d720bf17ff40913b8bdd4ae452b
praveenmudadla815 <praveenmudadla8115@gmail.com>
2024-04-18 18:48:43 +0530

    fetch method basic implementation using try catch

--- src/UsingFetchMethod/FetchMethod.js
+++ src/UsingFetchMethod/FetchMethod.js
@@ -3,19 +3,35 @@ import React, { useState, useEffect } from "react";
 const FetchMethod = () => {
   const [users, setUsers] = useState([]);
 
+  //   useEffect(() => {
+  //     fetch("https://jsonplaceholder.typicode.com/users")
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         setUsers(data);
+  //         console.log(data);
+  //       })
+  //       .catch((error) => console.error("Error:", error));
+  //   }, []);
+
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => {
+    const FatchUser = async () => {
+      try {
+        const response = await fetch(
+          "https://jsonplaceholder.typicode.com/users"
+        );
+        const data = await response.json();
         setUsers(data);
-        console.log(data);
-      })
-      .catch((error) => console.error("Error:", error));
+        console.log(data, "data");
+      } catch (error) {
+        console.error("data is error");
+      }
+    };
+    FatchUser();
   }, []);
   return (
     <div>
       {users.map((user) => (
-        <div>
+        <div key={user.id}>
           <h1>{user.name}</h1>
           <h3>{user.address.city}</h3>
         </div>
